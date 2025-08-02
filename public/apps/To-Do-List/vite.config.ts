b29c4656755a35d6a34a9c575e3dae89c8aae168
OldSci82 <122407819+OldSci82@users.noreply.github.com>
2025-08-02 13:58:18 -0400

    bug fixes with todo app

--- public/apps/To-Do-List/vite.config.ts
+++ public/apps/To-Do-List/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/public/apps/To-Do-List/', // Must match the exact subdirectory path
   root: 'src',
   build: {
     outDir: '../dist'
